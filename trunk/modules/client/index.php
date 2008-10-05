6e2f11bc0d7fd9d7561b904c3f384c4396f0b33c
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2008-10-05 11:12:53 +0000

    Client Authentication none, basic and openid

--- trunk/modules/client/index.php
+++ trunk/modules/client/index.php
@@ -17,7 +17,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-error_reporting(E_ALL|E_STRICT);
+// unfortunately the openid library does not yet support E_ALL nor E_STRICT
+//error_reporting(E_ALL|E_STRICT);
+error_reporting(E_ALL);
 
 define('CONFIG_FILE', 'config/config.ini');
 
@@ -35,6 +37,76 @@ if($config['misc']['display_errors'] == 1) {
 }
 else {
 	ini_set('display_errors', 0);
+}
+
+// check for Authentication
+if($config['client']['auth'] == 'basic') {
+	if(!isset($_SERVER['PHP_AUTH_USER']) || 
+		$_SERVER['PHP_AUTH_USER'] != $config['client']['auth_basic_username'] ||
+		$_SERVER['PHP_AUTH_PW'] != $config['client']['auth_basic_password']) {
+		header('HTTP/1.0 401 Unauthorized');
+		header('WWW-Authenticate: Basic realm="Origo Client"');
+		die('Origo Client error: 401 Unauthorized.');
+	}
+}
+else if($config['client']['auth'] == 'openid') {
+	session_start();
+	
+	if(!is_writeable('tmp')) {
+		die('Origo Client error: tmp directory is not writeable.');
+	}
+	if(!is_dir('tmp/openid')) {
+		mkdir('tmp/openid');
+		chmod('tmp/openid', 0775);
+	}
+
+	ini_set('include_path', 'libs/php-openid' . PATH_SEPARATOR . ini_get('include_path'));
+	require_once 'Auth/OpenID/Consumer.php';
+	require_once 'Auth/OpenID/FileStore.php';
+
+	$store = new Auth_OpenID_FileStore('tmp/openid');
+	$consumer = new Auth_OpenID_Consumer($store);
+	
+	$scheme = 'http';
+	if(isset($_SERVER['https']) && $_SERVER['https'] == 'on') {
+		$scheme .= 's';
+	}
+	$realm = $scheme . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/';
+	$return_to = $scheme . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['PHP_SELF'];	
+
+	$response = $consumer->complete($return_to);
+	if($response->status !== Auth_OpenID_SUCCESS) {	
+		$auth_request = $consumer->begin($config['client']['auth_openid']);
+	
+		if(!$auth_request) {
+			die('Origo Client error: Not a valid OpenID.');
+		}
+	
+		// For OpenID 1, send a redirect.  For OpenID 2, use a Javascript
+		// form to send a POST request to the server.
+		if($auth_request->shouldSendRedirect()) {
+			$redirect_url = $auth_request->redirectURL($realm, $return_to);
+			
+			if(Auth_OpenID::isFailure($redirect_url)) {
+				die('Origo Client error: Could not redirect to OpenID server.');
+			} 
+			else {
+				header('Location: ' . $redirect_url);
+			}
+		}
+		else {
+			// Generate form markup and render it.
+			$form_id = 'openid_message';
+			$form_html = $auth_request->htmlMarkup($realm, $return_to, false, array('id' => $form_id));
+	
+			if(Auth_OpenID::isFailure($form_html)) {
+					die('Origo Client error: Could not redirect to OpenID server.');
+			} 
+			else {
+				die($form_html);
+			}
+		}
+	}
 }
 
 // generate flash vars
