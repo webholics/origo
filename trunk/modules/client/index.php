f612bd19b5783e7571aa9fb3502f0ad5c550c2d8
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2008-10-15 16:23:47 +0000

    Added docs

--- trunk/modules/client/index.php
+++ trunk/modules/client/index.php
@@ -119,7 +119,7 @@ $flashVars = 'identifier=' . urlencode($config['client']['resource'])
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
-		<title></title>
+		<title>Origo</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<style type="text/css" media="screen">
 			html, body, #content	{ height:100%; }
