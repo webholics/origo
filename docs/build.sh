ada9ce0cb23ad8b50081911c52332dd53ecb75d2
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2008-10-20 08:11:44 +0000

    Docs: new directory structure

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,3 @@
- xsltproc --xinclude -o pdf/index.fo docbook-xsl-ns-1.74.0/fo/docbook.xsl docbook/index.xml
+ xsltproc --xinclude -o pdf/index.fo xsl/docbook-xsl-ns-1.74.0/fo/docbook.xsl docbook/index.xml
  fop pdf/index.fo pdf/index.pdf
- xsltproc --xinclude -o html/index.html docbook-xsl-ns-1.74.0/html/docbook.xsl docbook/index.xml
+ xsltproc --xinclude -o html/index.html xsl/docbook-xsl-ns-1.74.0/html/docbook.xsl docbook/index.xml
