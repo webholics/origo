ec6895be722b441924b95ce95f2ee93331a1205f
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2008-10-22 17:34:31 +0000

    First doc version ready.

--- rdf/index.php
+++ rdf/index.php
@@ -1,33 +1,24 @@
 <?php	
 /**
- * Origo 
+ * Origo - social client
+ * RDF distributor
+ *
  * Copyright (C) 2008 Mario Volke
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * All right reserved.
  */
 
 // unfortunately ARC2 does not yet support E_STRICT
 //error_reporting(E_ALL|E_STRICT);
 error_reporting(E_ALL);
 
-require_once 'libs/arc/ARC2.php';
+require_once '../libs/arc/ARC2.php';
 
-define('CONFIG_FILE', 'config/config.ini');
+define('CONFIG_FILE', '../config/config.ini');
+define('TMP_DIR', '../tmp');
 
 // check if configuration file exists
 if(!is_file(CONFIG_FILE)) {
-	die('Origo RDF Distributor error: Configuration file does not exist.');
+	die('Origo error: Configuration file does not exist.');
 }
 
 // load configuration from ini file
@@ -41,16 +32,16 @@ else {
 	ini_set('display_errors', 0);
 }
 
-if(!is_dir($config['cache']['dir'])) {
-	die('Origo RDF Distributor error: Cache directory does not exist.');
+if(!is_dir(TMP_DIR)) {
+	die('Origo error: Tmp directory does not exist.');
 }
 
-if(!is_readable($config['cache']['dir'])) {
-	die('Origo RDF Distributor error: Cache directory is not readable.');
+if(!is_readable(TMP_DIR)) {
+	die('Origo error: Tmp directory is not readable.');
 }
 
-if(!is_writable($config['cache']['dir'])) {
-	die('Origo RDF Distributor error: Cache directory is not writable.');
+if(!is_writable(TMP_DIR)) {
+	die('Origo error: Tmp directory is not writable.');
 }
 
 $file = $config['cache']['dir'] . '/' . $config['cache']['file'];
