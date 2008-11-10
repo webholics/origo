ca9b1f483333055051d5f00e5f7facbd4ef136c6
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2008-11-10 09:56:07 +0000

    Milestone 2 tagged

---
+++ tags/milestone-2/client/api/index.php
@@ -0,0 +1,71 @@
+<?php	
+/**
+ * Origo - social client
+ * SPARQL endpoint
+ *
+ * Copyright (C) 2008 Mario Volke
+ * All rights reserved.
+ */
+
+// unfortunately ARC2 does not yet support E_STRICT
+//error_reporting(E_ALL|E_STRICT);
+error_reporting(E_ALL);
+
+require_once '../../libs/arc2/ARC2.php';
+
+define('CONFIG_FILE', '../../config/config.ini');
+
+// check if configuration file exists
+if(!is_file(CONFIG_FILE)) {
+	die('Origo error: Configuration file does not exist.');
+}
+
+// load configuration from ini file
+$config = parse_ini_file(CONFIG_FILE, true);
+
+// should be disabled on prodution servers
+if($config['global']['display_errors'] == 1) {
+	ini_set('display_errors', 1);
+}
+else {
+	ini_set('display_errors', 0);
+}
+
+$endpoint_config = array(
+	// mysql database access
+	'db_host' => $config['database']['host'],
+	'db_name' => $config['database']['name'],
+	'db_user' => $config['database']['username'],
+	'db_pwd' => $config['database']['password'],
+
+	// store_name is used as table prefix
+	'store_name' => 'origo',
+
+	'endpoint_features' => array(
+		'select', 'construct', 'ask', 'describe', 
+		'load', 'insert', 'delete',
+		'dumb'
+	),
+
+	'endpoint_timeout' => 60, /* this feature is not yet implemented in ARC2 */
+	'endpoint_max_limit' => 5000,
+	'endpoint_read_key' => $config['client']['api_key'],
+	'endpoint_write_key' => $config['client']['api_key']
+);
+
+$ep = ARC2::getStoreEndpoint($endpoint_config);
+
+if(!$ep->isSetUp()) {
+	$ep->setUp();
+}
+
+if($errors = $ep->getErrors()) {
+	$out = '';
+	foreach($errors as $err) {
+		$out .= $err . "\n";
+	}
+	die($out);
+}
+
+$ep->go();
+
---
+++ tags/milestone-2/rdf/index.php
@@ -0,0 +1,96 @@
+<?php	
+/**
+ * Origo - social client
+ * RDF distributor
+ *
+ * Copyright (C) 2008 Mario Volke
+ * All right reserved.
+ */
+
+// unfortunately ARC2 does not yet support E_STRICT
+//error_reporting(E_ALL|E_STRICT);
+error_reporting(E_ALL);
+
+require_once '../libs/arc/ARC2.php';
+
+define('CONFIG_FILE', '../config/config.ini');
+define('TMP_DIR', '../tmp');
+
+// check if configuration file exists
+if(!is_file(CONFIG_FILE)) {
+	die('Origo error: Configuration file does not exist.');
+}
+
+// load configuration from ini file
+$config = parse_ini_file(CONFIG_FILE, true);
+
+// should be disabled on prodution servers
+if($config['global']['display_errors'] == 1) {
+	ini_set('display_errors', 1);
+}
+else {
+	ini_set('display_errors', 0);
+}
+
+if(!is_dir(TMP_DIR)) {
+	die('Origo error: Tmp directory does not exist.');
+}
+
+if(!is_readable(TMP_DIR)) {
+	die('Origo error: Tmp directory is not readable.');
+}
+
+if(!is_writable(TMP_DIR)) {
+	die('Origo error: Tmp directory is not writable.');
+}
+
+$file = $config['cache']['dir'] . '/' . $config['cache']['file'];
+// check if cache file is too old
+if(!is_file($file) || time() > filemtime($file) + $config['cache']['lifetime']) {
+	$store_config = array(
+		'remote_store_endpoint' => $config['endpoint']['location'] . '?key=' . urlencode($config['endpoint']['key'])
+	);
+
+	$store = ARC2::getRemoteStore($store_config);
+	
+	$query = "CONSTRUCT {"
+	       . "  ?s ?p ?o ."
+		   . "}"
+		   . "WHERE {"
+		   . "  GRAPH <" . $config['distributor']['resource'] . "> {"
+		   . "	  ?s ?p ?o ."
+		   . "  }"
+		   . "}";
+
+	$index = $store->query($query, 'raw');
+
+	// we want nice namespaces
+	$ns = array(
+		'foaf' => 'http://xmlns.com/foaf/0.1/',
+		'dc' => 'http://purl.org/dc/elements/1.1/',
+		'owl' => 'http://www.w3.org/2002/07/owl#',
+		'vcard' => 'http://www.w3.org/2001/vcard-rdf/3.0#',
+		'skos' => 'http://www.w3.org/2004/02/skos/core#',
+		'sioc' => 'http://rdfs.org/sioc/ns#',
+		'xfn' => 'http://gmpg.org/xfn/11#'
+	);
+
+	$ser = ARC2::getRDFXMLSerializer(array('ns' => $ns));
+	$doc = $ser->getSerializedIndex($index);
+	
+	if($errors = $store->getErrors()) {
+		$out = '';
+		foreach($errors as $err) {
+			$out .= $err . "\n";
+		}
+		die($out);
+	}
+
+	if(file_put_contents($file, $doc) === false) {
+		die('Origo RDF Distributor error: Could not write cache file.');
+	}
+}
+
+header('Content-Type: application/rdf+xml;charset=utf-8');
+echo file_get_contents($file);
+
