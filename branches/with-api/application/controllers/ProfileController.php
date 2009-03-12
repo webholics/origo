c4708b0f315ecc4fecf7641edd78ac772681917e
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2009-03-12 12:14:20 +0000

    added several serializations

--- branches/with-api/application/controllers/ProfileController.php
+++ branches/with-api/application/controllers/ProfileController.php
@@ -30,23 +30,30 @@ class ProfileController extends BaseController
 		// content negotiation
 		$mime_types = array(
 			'type' => array(
-				'application/rdf+xml'	
+				'application/rdf+xml',
+				'application/turtle',
+				'application/json',
+				'text/html'
 			),
 			'app_preference' => array(
-				1.0	
+				1.0,
+				0.6,
+				0.6,
+				0.8
 			)
 		);
 		$uris = array(
-			'application/rdf+xml' => $this->view->url(array(), 'profile_rdf')	
+			'application/rdf+xml' => $this->view->url(array(), 'profile_rdf'),
+			'application/turtle' => $this->view->url(array(), 'profile_turtle'),	
+			'application/json' => $this->view->url(array(), 'profile_json')	
 		);
 
 		// add html ressource
 		$config = $this->getConfig();
-		if(!empty($config->negotiation->html)) {
-			$mime_types['type'][] = 'text/html';
-			$mime_types['app_preference'][] = 0.8;
+		if(!empty($config->negotiation->html))
 			$uris['text/html'] = $config->negotiation->html;
-		}
+		else
+			$uris['text/html'] = $this->view->url(array(), 'profile_html');
 
 		$mime_best = content_negotiation::mime_best_negotiation($mime_types);
 
@@ -58,7 +65,7 @@ class ProfileController extends BaseController
 	}
 
 	/**
-	 * Publish RDF profile.
+	 * Publish RDF/XML profile.
 	 * Profile will be cached as configured.
 	 */
 	public function rdfAction()
@@ -118,5 +125,192 @@ class ProfileController extends BaseController
 
 		header('Content-Type: application/rdf+xml;charset=utf-8');
 		echo $doc;
+	}
+
+	/**
+	 * Publish Turtle profile.
+	 * Profile will be cached as configured.
+	 */
+	public function turtleAction()
+	{
+		$cache = $this->getCache();
+		$cacheId = 'profile_turtle';
+		if(!$doc = $cache->load($cacheId)) {
+			$config = $this->getConfig();
+			$store = $this->getProfileStore();
+
+			$query = '
+				CONSTRUCT { 
+					?s ?p ?o .
+				}
+				WHERE {
+					?s ?p ?o .
+				}
+			';
+
+			$index = $store->query($query, 'raw');
+
+			// we want nice namespaces
+			$ns = array(
+				'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+				'foaf' => 'http://xmlns.com/foaf/0.1/',
+				'dc' => 'http://purl.org/dc/elements/1.1/',
+				'dct' => 'http://purl.org/dc/terms/',
+				'owl' => 'http://www.w3.org/2002/07/owl#',
+				'vcard' => 'http://www.w3.org/2001/vcard-rdf/3.0#',
+				'skos' => 'http://www.w3.org/2004/02/skos/core#',
+				'sioc' => 'http://rdfs.org/sioc/ns#',
+				'xfn' => 'http://gmpg.org/xfn/11#',
+				'rel' => 'http://purl.org/vocab/relationship/'
+			);
+
+			$serializer_config = array(
+				'ns' => $ns
+			);
+
+			$ser = ARC2::getTurtleSerializer($serializer_config);
+			$doc = $ser->getSerializedIndex($index);
+				
+			if($errors = $store->getErrors()) {
+				if($config->misc->environment == 'development') {
+					$out = '';
+					foreach($errors as $err) {
+						$out .= $err . "\n";
+					}
+					echo $out;
+				}
+				die();
+			}
+
+			$cache->save($doc, $cacheId, array('profile'));
+		}
+
+		header('Content-Type: application/turtle;charset=utf-8');
+		echo $doc;
+	}
+
+	/**
+	 * Publish RDF/JSON profile.
+	 * Profile will be cached as configured.
+	 */
+	public function jsonAction()
+	{
+		$cache = $this->getCache();
+		$cacheId = 'profile_json';
+		if(!$doc = $cache->load($cacheId)) {
+			$config = $this->getConfig();
+			$store = $this->getProfileStore();
+
+			$query = '
+				CONSTRUCT { 
+					?s ?p ?o .
+				}
+				WHERE {
+					?s ?p ?o .
+				}
+			';
+
+			$index = $store->query($query, 'raw');
+
+			// we want nice namespaces
+			$ns = array(
+				'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+				'foaf' => 'http://xmlns.com/foaf/0.1/',
+				'dc' => 'http://purl.org/dc/elements/1.1/',
+				'dct' => 'http://purl.org/dc/terms/',
+				'owl' => 'http://www.w3.org/2002/07/owl#',
+				'vcard' => 'http://www.w3.org/2001/vcard-rdf/3.0#',
+				'skos' => 'http://www.w3.org/2004/02/skos/core#',
+				'sioc' => 'http://rdfs.org/sioc/ns#',
+				'xfn' => 'http://gmpg.org/xfn/11#',
+				'rel' => 'http://purl.org/vocab/relationship/'
+			);
+
+			$serializer_config = array(
+				'ns' => $ns
+			);
+
+			$ser = ARC2::getRDFJSONSerializer($serializer_config);
+			$doc = $ser->getSerializedIndex($index);
+				
+			if($errors = $store->getErrors()) {
+				if($config->misc->environment == 'development') {
+					$out = '';
+					foreach($errors as $err) {
+						$out .= $err . "\n";
+					}
+					echo $out;
+				}
+				die();
+			}
+
+			$cache->save($doc, $cacheId, array('profile'));
+		}
+
+		header('Content-Type: application/json;charset=utf-8');
+		echo $doc;
+	}
+
+	/**
+	 * Publish POSH RDF profile.
+	 * Profile will be cached as configured.
+	 */
+	public function htmlAction()
+	{
+		$cache = $this->getCache();
+		$cacheId = 'profile_html';
+		if(!$doc = $cache->load($cacheId)) {
+			$config = $this->getConfig();
+			$store = $this->getProfileStore();
+
+			$query = '
+				CONSTRUCT { 
+					?s ?p ?o .
+				}
+				WHERE {
+					?s ?p ?o .
+				}
+			';
+
+			$index = $store->query($query, 'raw');
+
+			// we want nice namespaces
+			$ns = array(
+				'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+				'foaf' => 'http://xmlns.com/foaf/0.1/',
+				'dc' => 'http://purl.org/dc/elements/1.1/',
+				'dct' => 'http://purl.org/dc/terms/',
+				'owl' => 'http://www.w3.org/2002/07/owl#',
+				'vcard' => 'http://www.w3.org/2001/vcard-rdf/3.0#',
+				'skos' => 'http://www.w3.org/2004/02/skos/core#',
+				'sioc' => 'http://rdfs.org/sioc/ns#',
+				'xfn' => 'http://gmpg.org/xfn/11#',
+				'rel' => 'http://purl.org/vocab/relationship/'
+			);
+
+			$serializer_config = array(
+				'ns' => $ns
+			);
+
+			$ser = ARC2::getPOSHRDFSerializer($serializer_config);
+			$doc = $ser->getSerializedIndex($index);
+				
+			if($errors = $store->getErrors()) {
+				if($config->misc->environment == 'development') {
+					$out = '';
+					foreach($errors as $err) {
+						$out .= $err . "\n";
+					}
+					echo $out;
+				}
+				die();
+			}
+
+			$cache->save($doc, $cacheId, array('profile'));
+		}
+
+		$this->view->doc = $doc;
+		$this->view->identifier = $this->getIdentifier();
+		$this->render();
 	}
 }
--- branches/with-api/application/routes.php
+++ branches/with-api/application/routes.php
@@ -17,5 +17,14 @@ $router->addRoute('profile', new Zend_Controller_Router_Route('/',
 $router->addRoute('profile_rdf', new Zend_Controller_Router_Route('rdf',
 	array('controller' => 'profile', 'action' => 'rdf')
 ));
+$router->addRoute('profile_turtle', new Zend_Controller_Router_Route('ttl',
+	array('controller' => 'profile', 'action' => 'turtle')
+));
+$router->addRoute('profile_json', new Zend_Controller_Router_Route('json',
+	array('controller' => 'profile', 'action' => 'json')
+));
+$router->addRoute('profile_html', new Zend_Controller_Router_Route('html',
+	array('controller' => 'profile', 'action' => 'html')
+));
 
 unset($router);
