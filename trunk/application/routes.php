351d1a1a6ec736b5e8403d31995f4845d5f82bf5
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2009-04-12 13:43:41 +0000

    bug with case-sensitivity in routes.php solved

--- trunk/application/routes.php
+++ trunk/application/routes.php
@@ -39,45 +39,45 @@ $router->addRoute('api_test', new Zend_Controller_Router_Route('api/test',
 
 // EditorApiController
 $router->addRoute('api_editor_get', new Zend_Controller_Router_Route('api/editor/get',
-	array('controller' => 'editorApi', 'action' => 'get')
+	array('controller' => 'editor-api', 'action' => 'get')
 ));
 $router->addRoute('api_editor_update', new Zend_Controller_Router_Route('api/editor/update',
-	array('controller' => 'editorApi', 'action' => 'update')
+	array('controller' => 'editor-api', 'action' => 'update')
 ));
 $router->addRoute('api_editor_delete', new Zend_Controller_Router_Route('api/editor/delete',
-	array('controller' => 'editorApi', 'action' => 'delete')
+	array('controller' => 'editor-api', 'action' => 'delete')
 ));
 $router->addRoute('api_editor_clean', new Zend_Controller_Router_Route('api/editor/clean',
-	array('controller' => 'editorApi', 'action' => 'clean')
+	array('controller' => 'editor-api', 'action' => 'clean')
 ));
 $router->addRoute('api_editor_profiles_get', new Zend_Controller_Router_Route('api/editor/profiles/get',
-	array('controller' => 'editorApi', 'action' => 'profilesget')
+	array('controller' => 'editor-api', 'action' => 'profilesget')
 ));
 $router->addRoute('api_editor_profiles_update', new Zend_Controller_Router_Route('api/editor/profiles/update',
-	array('controller' => 'editorApi', 'action' => 'profilesupdate')
+	array('controller' => 'editor-api', 'action' => 'profilesupdate')
 ));
 $router->addRoute('api_editor_profiles_delete', new Zend_Controller_Router_Route('api/editor/profiles/delete',
-	array('controller' => 'editorApi', 'action' => 'profilesdelete')
+	array('controller' => 'editor-api', 'action' => 'profilesdelete')
 ));
 $router->addRoute('api_editor_relationships_get', new Zend_Controller_Router_Route('api/editor/relationships/get',
-	array('controller' => 'editorApi', 'action' => 'relationshipsget')
+	array('controller' => 'editor-api', 'action' => 'relationshipsget')
 ));
 $router->addRoute('api_editor_relationships_update', new Zend_Controller_Router_Route('api/editor/relationships/update',
-	array('controller' => 'editorApi', 'action' => 'relationshipsupdate')
+	array('controller' => 'editor-api', 'action' => 'relationshipsupdate')
 ));
 $router->addRoute('api_editor_relationships_delete', new Zend_Controller_Router_Route('api/editor/relationships/delete',
-	array('controller' => 'editorApi', 'action' => 'relationshipsdelete')
+	array('controller' => 'editor-api', 'action' => 'relationshipsdelete')
 ));
 
 // BrowserApiController
 $router->addRoute('api_browser_profile', new Zend_Controller_Router_Route('api/browser/profile',
-	array('controller' => 'browserApi', 'action' => 'profile')
+	array('controller' => 'browser-api', 'action' => 'profile')
 ));
 $router->addRoute('api_browser_relationships', new Zend_Controller_Router_Route('api/browser/relationships',
-	array('controller' => 'browserApi', 'action' => 'relationships')
+	array('controller' => 'browser-api', 'action' => 'relationships')
 ));
 $router->addRoute('api_browser_clean', new Zend_Controller_Router_Route('api/browser/clean',
-	array('controller' => 'browserApi', 'action' => 'clean')
+	array('controller' => 'browser-api', 'action' => 'clean')
 ));
 
 unset($router);
