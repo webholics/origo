db67d5d4e80c3ec4344b49074253700a76c69969
mvolke <mvolke@e27f8668-2815-11de-a7ed-71eef27883a5>
2009-04-13 09:27:37 +0000

    docs deleted in demo branch

--- docs/build.sh
+++
@@ -1,3 +0,0 @@
- xsltproc --xinclude -o pdf/index.fo xsl/docbook-xsl-ns-1.74.0/fo/docbook.xsl docbook/index.xml
- fop pdf/index.fo pdf/index.pdf
- xsltproc --xinclude -o xhtml/index.html xsl/xhtml.xsl docbook/index.xml
